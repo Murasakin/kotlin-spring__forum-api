dc2af903269758931ae4d4d36071614566c3baf7
Gustavo Fernando Balbino <gustavo.balbino@pca.com.br>
2021-09-27 14:44:38 -0300

    primeira parte completa

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,8 +5,13 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
-        @field:NotEmpty @field:Size(min=5, max=100) val titulo: String,
-        @field:NotEmpty val mensagem: String,
-        @field:NotNull val idCurso: Long,
-        @field:NotNull val idAutor: Long
+        @field:NotEmpty(message = "Título não pode ser em branco")
+        @field:Size(min=5, max=100, message = "Título deve ter entre 5 e 100 caractéres")
+        val titulo: String,
+        @field:NotEmpty(message = "Mensagem não pode ser em branco")
+        val mensagem: String,
+        @field:NotNull
+        val idCurso: Long,
+        @field:NotNull
+        val idAutor: Long
         )
